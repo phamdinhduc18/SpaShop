b501ac8775216cbdd11f76c939920378bd2fdcdb
ĐỨC PHẠM ĐÌNH <16520245@ms.uit.edu.vn>
2021-05-21 00:04:03 +0700

    Create Ef Core

---
+++ SpaShop.Data/EF/SpaShopDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpaShop.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaShop.Data.EF
+{
+    public class SpaShopDbContext : DbContext
+    {
+        public SpaShopDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products;
+        public DbSet<Category> Categories;
+    }
+}
